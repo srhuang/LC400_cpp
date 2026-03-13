ba0585fc80924ddb4dcbd0334d30558e49aa04a4
srhuang <lukyandy3162@gmail.com>
2026-03-14 02:55:07 +0800

    026

--- Array/026_Remove_Duplicates_from_Sorted_Array/removeDuplicates.cpp
+++ Array/026_Remove_Duplicates_from_Sorted_Array/removeDuplicates.cpp
@@ -7,13 +7,13 @@ public:
     int removeDuplicates(vector<int> &nums){
         if(nums.size() == 0)
             return 0;
-        int cnt = 0;
+        int j = 0;
         for(int i = 1; i < nums.size(); i++){
-            if(nums[cnt] != nums[i]){
-                nums[++cnt] = nums[i];
+            if(nums[j] != nums[i]){
+                nums[++j] = nums[i];
             }
         }
-        return cnt + 1;
+        return j + 1;
     }
 };
 
