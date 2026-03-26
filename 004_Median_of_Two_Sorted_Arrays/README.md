dda08f12608e9959d7bc384283a7bf2b4543ad4d
srhuang <lukyandy3162@gmail.com>
2026-03-26 18:11:39 +0800

    004

--- 004_Median_of_Two_Sorted_Arrays/README.md
+++ 004_Median_of_Two_Sorted_Arrays/README.md
@@ -2,12 +2,22 @@
 ===
 
 # Explanation
-
+* 給定兩個已排序的 Array: n1, n2。
+* 找出中位數。
+* 時間複雜度為 $O(log(n1+n2))$
 
 # Approach
+* 利用 binary search 來找尋適當的 m1。
+* 中位數的 index 可以先算好，因此給定 n1, n2，就可以決定中位數 index k。
+* 注意 binary search 是在什麼條件下把 m1 剔除。
+* m1 or m2 等於 0 表示完全不取該陣列數值。
+* m1 or m2 等於 size 表示完全取該陣列的數值。
+* 需要考慮總個數是奇數還是偶數。
+* 這題困難的是邊際條件很複雜。
+* 採用 [https://zxi.mytechroad.com/blog/algorithms/binary-search/leetcode-4-median-of-two-sorted-arrays/](花花酱 LeetCode 4. Median of Two Sorted Arrays)
 
 ## Complexity
-* Time:
-* Space:
+* Time: $O(log(min(n1, n2)))$
+* Space: $O(1)$
 
 
--- 004_Median_of_Two_Sorted_Arrays/findMedianSortedArrays_1.cpp
+++ 004_Median_of_Two_Sorted_Arrays/findMedianSortedArrays_1.cpp
@@ -5,11 +5,62 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // make sure nums1 is less than nums2
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int k = (n1 + n2 + 1) / 2;
+
+        // binary search for m1
+        int left = 0;
+        int right = n1;
+        while (left < right)
+        {
+            int m1 = left + (right - left) / 2;
+            int m2 = k - m1;
+            // check m1
+            if (nums1[m1] < nums2[m2 - 1]) {
+                left = m1 + 1;
+            } else {
+                right = m1;
+            }
+        }
+
+        // calculate median
+        int m1 = left;
+        int m2 = k - m1;
+        int c1 = max(m1 == 0 ? INT_MIN : nums1[m1 - 1], 
+                     m2 == 0 ? INT_MIN : nums2[m2 - 1]);
+        // odd
+        if ((n1 + n2) % 2 == 1) {
+            return c1;
+        }
+
+        // even
+        int c2 = min(m1 == n1 ? INT_MAX : nums1[m1],
+                     m2 == n2 ? INT_MAX : nums2[m2]);
+        return (c1 + c2) * 0.5;
     }
 };
 
 int main(int argc, char* argv[])
 {
+    // input data
+    int ary1[] = {1, 2};
+    int ary2[] = {3, 4};
+    int m = sizeof(ary1) / sizeof(ary1[0]);
+    int n = sizeof(ary2) / sizeof(ary2[0]);
+    vector<int> nums1(ary1, ary1 + m);
+    vector<int> nums2(ary2, ary2 + n);
+
+    // solution
+    Solution s;
+    double ans = s. findMedianSortedArrays(nums1, nums2);
+
+    // print
+    cout << "Answer: " << ans << endl;
 
 }
